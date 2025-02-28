7ef9e0ffadc7e661c067382f56f80d4cfe6350f2
Olivia Christensen <oliviapc09@gmail.com>
2025-02-27 18:44:45 -0700

    syntax error

--- Mission8Assignment_Group310/obj/Debug/net8.0/Mission8Assignment_Group310.AssemblyInfo.cs
+++ Mission8Assignment_Group310/obj/Debug/net8.0/Mission8Assignment_Group310.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission8Assignment_Group310")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9b1c469a518c0d12a7df9bffe767a03bcb38a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db7aa4248cc5eb40a8a5c4698347ec37dff9e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission8Assignment_Group310")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission8Assignment_Group310")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
